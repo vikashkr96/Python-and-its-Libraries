dfff453ea7565e615174215899abf6f2272a7de6
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2025-12-19 16:13:36 +0530

    Update checknumofdim.py

--- Numpy/checknumofdim.py
+++ Numpy/checknumofdim.py
@@ -1,6 +1,7 @@
-#to check the no of dimentions........ 
+#to check the dimentions........ 
 import numpy as np
 
 arr=np.array([[[1, 2, 3], [4, 5, 6], [7,8,9]], [[1, 2, 3], [4, 5, 6], [7,8,9]]]) 
 
-print(arr.ndim)             # function that is used to check the no of dimentions
+print(arr.ndim)             # function that is used to check the  dimentions
+
