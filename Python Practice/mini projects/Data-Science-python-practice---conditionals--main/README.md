de57d3374bb9a85f6029815f0709c1211a6b2f0e
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-01-13 13:06:23 +0530

    Add files via upload

---
+++ Python Practice/mini projects/Data-Science-python-practice---conditionals--main/README.md
@@ -0,0 +1,11 @@
+# Data-Science-python-practice---conditionals-
+
+* **Write a Python program that takes an integer as input and prints "Even" if it is even, otherwise prints "Odd".** 
+
+* **Write a Python program that takes a number as input and prints whether it is positive, negative, or zero.**
+
+* you can check the code by clicking on the file "task_day_2.py"
+
+   
+
+
---
+++ Python Practice/mini projects/Data-Science-python-practice---conditionals--main/task_day_2.py
@@ -0,0 +1,21 @@
+# # Write a Python program that takes an integer as input and prints "Even" if it is even, otherwise prints "Odd".
+# a= int(input("Enter a number:- "))
+# if a % 2 == 0:
+#     print(f"{a} is an even number")
+# else:
+#     print(f"{a} is an odd number")
+
+
+# # Write a Python program that takes a number as input and prints whether it is positive, negative, or zero.
+    
+# a= int(input("Enter a number:- "))
+# if a == 0:
+#     print('The number entered is zero')
+# elif a < 0:
+#     print('The number entered is negative ')
+# elif a > 0:
+#     print('The number entered is positive ')
+# else:
+#     print("invalid input!!")
+
+
